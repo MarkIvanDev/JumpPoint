478ada708d797e82e590bcf8fc74cc1adc9972ec
Mark Ivan Basto <markivan.developer@outlook.com>
2021-02-27 16:26:30 +0800

    add core properties

---
+++ src/JumpPoint/JumpPoint.Platform.Items/Storage/Properties/Core/FlagColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpPoint.Platform.Items.Storage.Properties.Core
+{
+    public enum FlagColor : ushort
+    {
+        None = 0,
+        Purple = 1,
+        Orange = 2,
+        Green = 3,
+        Yellow = 4,
+        Blue = 5,
+        Red = 6
+    }
+}
---
+++ src/JumpPoint/JumpPoint.Platform.Items/Storage/Properties/Core/Sensitivity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpPoint.Platform.Items.Storage.Properties.Core
+{
+    public enum Sensitivity : ushort
+    {
+        Normal = 0,
+        Personal = 1,
+        Private = 2,
+        Confidential = 3
+    }
+}
