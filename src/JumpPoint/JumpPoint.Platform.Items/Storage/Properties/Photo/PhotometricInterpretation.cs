967d20807cb0fe83a4ad7eeb7c3aeb91bca48b7d
Mark Ivan Basto <markivan.developer@outlook.com>
2021-02-28 19:00:48 +0800

    add photo properties

---
+++ src/JumpPoint/JumpPoint.Platform.Items/Storage/Properties/Photo/PhotometricInterpretation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace JumpPoint.Platform.Items.Storage.Properties.Photo
+{
+    public enum PhotometricInterpretation : ushort
+    {
+        RGB = 2,
+        [Description("YCbCr")]
+        YCbCr = 6
+    }
+}
