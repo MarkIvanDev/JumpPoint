17dfb5eabbbc02c9fa77df65f903f4697a139c6d
Mark Ivan Basto <markivan.developer@outlook.com>
2021-02-28 16:10:10 +0800

    add music properties

--- src/JumpPoint/JumpPoint.Platform.Items/Storage/Properties/MusicProperties.cs
+++ src/JumpPoint/JumpPoint.Platform.Items/Storage/Properties/MusicProperties.cs
@@ -23,25 +23,25 @@ namespace JumpPoint.Platform.Items.Storage.Properties
             set { Set(ref _album, value); }
         }
 
-        private IList<string> _artist;
+        private string[] _artists;
 
-        public IList<string> Artists
+        public string[] Artists
         {
-            get { return _artist; }
-            set { Set(ref _artist, value); }
+            get { return _artists; }
+            set { Set(ref _artists, value); }
         }
 
-        private IList<string> _composers;
+        private string[] _composers;
 
-        public IList<string> Composers
+        public string[] Composers
         {
             get { return _composers; }
             set { Set(ref _composers, value); }
         }
 
-        private IList<string> _conductors;
+        private string[] _conductors;
 
-        public IList<string> Conductors
+        public string[] Conductors
         {
             get { return _conductors; }
             set { Set(ref _conductors, value); }
@@ -63,6 +63,22 @@ namespace JumpPoint.Platform.Items.Storage.Properties
             set { Set(ref _trackNumber, value); }
         }
 
+        private bool? _isCompilation;
+
+        public bool? IsCompilation
+        {
+            get { return _isCompilation; }
+            set { Set(ref _isCompilation, value); }
+        }
+
+        private string _partOfSet;
+
+        public string PartOfSet
+        {
+            get { return _partOfSet; }
+            set { Set(ref _partOfSet, value); }
+        }
+
         private string _beatsPerMinute;
 
         public string BeatsPerMinute
@@ -71,9 +87,9 @@ namespace JumpPoint.Platform.Items.Storage.Properties
             set { Set(ref _beatsPerMinute, value); }
         }
 
-        private IList<string> _genre;
+        private string[] _genre;
 
-        public IList<string> Genre
+        public string[] Genre
         {
             get { return _genre; }
             set { Set(ref _genre, value); }
@@ -103,23 +119,44 @@ namespace JumpPoint.Platform.Items.Storage.Properties
             set { Set(ref _period, value); }
         }
 
+        private string _lyrics;
+
+        public string Lyrics
+        {
+            get { return _lyrics; }
+            set { Set(ref _lyrics, value); }
+        }
+
+        private byte[] _synchronizedLyrics;
+
+        public byte[] SynchronizedLyrics
+        {
+            get { return _synchronizedLyrics; }
+            set { Set(ref _synchronizedLyrics, value); }
+        }
+
         public static MusicProperties Extract(IDictionary<string, object> props)
         {
             var musicProperties = new MusicProperties()
             {
                 AlbumArtist = (string)props[Key.AlbumArtist],
                 Album = (string)props[Key.Album],
-                Artists = props[Key.Artists] as IList<string> ?? new List<string>(),
-                Composers = props[Key.Composers] as IList<string> ?? new List<string>(),
-                Conductors = props[Key.Conductors] as IList<string> ?? new List<string>(),
+                Artists = props[Key.Artists] as string[] ?? new string[0],
+                Composers = props[Key.Composers] as string[] ?? new string[0],
+                Conductors = props[Key.Conductors] as string[] ?? new string[0],
                 DiscNumber = props[Key.DiscNumber] as uint?,
                 TrackNumber = props[Key.TrackNumber] as uint?,
+                IsCompilation = props[Key.IsCompilation] as bool?,
+                PartOfSet = (string)props[Key.PartOfSet],
 
                 BeatsPerMinute = (string)props[Key.BeatsPerMinute],
-                Genre = props[Key.Genre] as IList<string> ?? new List<string>(),
+                Genre = props[Key.Genre] as string[] ?? new string[0],
                 InitialKey = (string)props[Key.InitialKey],
                 Mood = (string)props[Key.Mood],
-                Period = (string)props[Key.Period]
+                Period = (string)props[Key.Period],
+
+                Lyrics = (string)props[Key.Lyrics],
+                SynchronizedLyrics = props[Key.SynchronizedLyrics] as byte[]
             };
             return musicProperties;
         }
@@ -133,12 +170,17 @@ namespace JumpPoint.Platform.Items.Storage.Properties
             public static string Conductors => "System.Music.Conductor";
             public static string DiscNumber => "System.Music.DiscNumber";
             public static string TrackNumber => "System.Music.TrackNumber";
+            public static string IsCompilation => "System.Music.IsCompilation";
+            public static string PartOfSet => "System.Music.PartOfSet";
 
             public static string BeatsPerMinute => "System.Music.BeatsPerMinute";
             public static string Genre => "System.Music.Genre";
             public static string InitialKey => "System.Music.InitialKey";
             public static string Mood => "System.Music.Mood";
             public static string Period => "System.Music.Period";
+
+            public static string Lyrics => "System.Music.Lyrics";
+            public static string SynchronizedLyrics => "System.Music.SynchronizedLyrics";
 
             public static IEnumerable<string> All()
             {
@@ -149,12 +191,17 @@ namespace JumpPoint.Platform.Items.Storage.Properties
                 yield return Conductors;
                 yield return DiscNumber;
                 yield return TrackNumber;
+                yield return IsCompilation;
+                yield return PartOfSet;
                 
                 yield return BeatsPerMinute;
                 yield return Genre;
                 yield return InitialKey;
                 yield return Mood;
                 yield return Period;
+
+                yield return Lyrics;
+                yield return SynchronizedLyrics;
             }
         }
     }
