fdd6f73d91e5a4fac28d881be196e44dfce712d8
Mark Ivan Basto <markivan.developer@outlook.com>
2021-05-22 18:41:14 +0800

    add Jump List service

---
+++ src/JumpPoint/JumpPoint.Platform.Services/JumpList/JumpListService.netstandard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpPoint.Platform.Services
+{
+    public static partial class JumpListService
+    {
+        static Task PlatformInitialize()
+            => throw new NotImplementedException();
+    }
+}
---
+++ src/JumpPoint/JumpPoint.Platform.Services/JumpList/JumpListService.shared.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpPoint.Platform.Services
+{
+    public static partial class JumpListService
+    {
+
+        public static async Task Initialize()
+            => await PlatformInitialize();
+
+    }
+}
