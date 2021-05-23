d0fbecaa1f1d992ff80eaf6d3541484f3388b774
Mark Ivan Basto <markivan.developer@outlook.com>
2021-05-23 20:28:44 +0800

    add AppPath

---
+++ src/JumpPoint/JumpPoint.Platform/AppPath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpPoint.Platform
+{
+    public enum AppPath
+    {
+        Unknown = 0,
+
+        Dashboard = 1,
+        Settings = 2,
+        Favorites = 3,
+        Drives = 4,
+        CloudDrives = 5,
+        Workspaces = 6,
+        AppLinks = 7,
+
+        Drive = 22,
+        Folder = 23,
+        Workspace = 24,
+        Cloud = 25,
+
+        Properties = 41
+    }
+}
