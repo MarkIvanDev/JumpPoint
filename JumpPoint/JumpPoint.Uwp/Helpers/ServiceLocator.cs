99ec0e97e3c382c167e1aba87f687ebd69b61cf2
Mark Ivan Basto <markivan.developer@outlook.com>
2022-02-05 11:40:27 +0800

    fix crash when launching built-in tools

--- JumpPoint/JumpPoint.Uwp/Helpers/ServiceLocator.cs
+++ JumpPoint/JumpPoint.Uwp/Helpers/ServiceLocator.cs
@@ -17,66 +17,72 @@ namespace JumpPoint.Uwp.Helpers
         public ServiceLocator()
         {
             // Register DialogService
-            var dialogService = new DialogService();
-            dialogService.Configure(DialogKeys.RequestPermission, typeof(Dialogs.RequestPermissionDialog));
-            dialogService.Configure(DialogKeys.DeleteConfirmation, typeof(Dialogs.DeleteConfirmationDialog));
-            dialogService.Configure(DialogKeys.NewFolder, typeof(Dialogs.NewFolderDialog));
-            dialogService.Configure(DialogKeys.NewWorkspace, typeof(Dialogs.AddWorkspaceDialog));
-            //dialogService.Configure(DialogKeys.NewAppLink, typeof(Dialogs.NewAppLinkDialog));
-            dialogService.Configure(DialogKeys.Rename, typeof(Dialogs.RenameDialog));
-            dialogService.Configure(DialogKeys.AddToWorkspace, typeof(Dialogs.AddToWorkspaceDialog));
-            dialogService.Configure(DialogKeys.AppLinkLaunch, typeof(Dialogs.AppLinkLaunchDialog));
-            dialogService.Configure(DialogKeys.AppLinkLaunchResults, typeof(Dialogs.AppLinkLaunchResultsDialog));
-            dialogService.Configure(DialogKeys.WorkspaceTemplatePicker, typeof(Dialogs.WorkspaceTemplatePicker));
-            dialogService.Configure(DialogKeys.FolderTemplatePicker, typeof(Dialogs.FolderTemplatePicker));
-            dialogService.Configure(DialogKeys.RenameCloudAccount, typeof(Dialogs.RenameCloudAccountDialog));
-            dialogService.Configure(DialogKeys.AppLinkProviderPicker, typeof(Dialogs.AppLinkProviderPicker));
-            dialogService.Configure(DialogKeys.ToolPicker, typeof(Dialogs.ToolPicker));
-            dialogService.Configure(DialogKeys.Copy, typeof(Dialogs.Clipboard.CopyDialog));
-            dialogService.Configure(DialogKeys.Move, typeof(Dialogs.Clipboard.MoveDialog));
-            SimpleIoc.Default.Register<IDialogService>(() => dialogService);
+            SimpleIoc.Default.TryRegister<IDialogService>(() =>
+            {
+                var dialogService = new DialogService();
+                dialogService.Configure(DialogKeys.RequestPermission, typeof(Dialogs.RequestPermissionDialog));
+                dialogService.Configure(DialogKeys.DeleteConfirmation, typeof(Dialogs.DeleteConfirmationDialog));
+                dialogService.Configure(DialogKeys.NewFolder, typeof(Dialogs.NewFolderDialog));
+                dialogService.Configure(DialogKeys.NewWorkspace, typeof(Dialogs.AddWorkspaceDialog));
+                //dialogService.Configure(DialogKeys.NewAppLink, typeof(Dialogs.NewAppLinkDialog));
+                dialogService.Configure(DialogKeys.Rename, typeof(Dialogs.RenameDialog));
+                dialogService.Configure(DialogKeys.AddToWorkspace, typeof(Dialogs.AddToWorkspaceDialog));
+                dialogService.Configure(DialogKeys.AppLinkLaunch, typeof(Dialogs.AppLinkLaunchDialog));
+                dialogService.Configure(DialogKeys.AppLinkLaunchResults, typeof(Dialogs.AppLinkLaunchResultsDialog));
+                dialogService.Configure(DialogKeys.WorkspaceTemplatePicker, typeof(Dialogs.WorkspaceTemplatePicker));
+                dialogService.Configure(DialogKeys.FolderTemplatePicker, typeof(Dialogs.FolderTemplatePicker));
+                dialogService.Configure(DialogKeys.RenameCloudAccount, typeof(Dialogs.RenameCloudAccountDialog));
+                dialogService.Configure(DialogKeys.AppLinkProviderPicker, typeof(Dialogs.AppLinkProviderPicker));
+                dialogService.Configure(DialogKeys.ToolPicker, typeof(Dialogs.ToolPicker));
+                dialogService.Configure(DialogKeys.Copy, typeof(Dialogs.Clipboard.CopyDialog));
+                dialogService.Configure(DialogKeys.Move, typeof(Dialogs.Clipboard.MoveDialog));
+                return dialogService;
+            });
 
             // Register AddOnService
-            var addOnService = new AddOnService();
+            SimpleIoc.Default.TryRegister<IAddOnService>(() =>
+            {
+                var addOnService = new AddOnService();
 #if BETA
-            addOnService.Configure(AddOnKeys.Monthly1, new SubscriptionAddOn("9P2WC0V906ZL") { Price = "$0.99" });
-            addOnService.Configure(AddOnKeys.Monthly2, new SubscriptionAddOn("9PLBZKNFTCL0") { Price = "$2.99" });
-            addOnService.Configure(AddOnKeys.Monthly3, new SubscriptionAddOn("9PJ1PJ9SHQ2G") { Price = "$4.99" });
-            addOnService.Configure(AddOnKeys.Monthly4, new SubscriptionAddOn("9P17MRR2QMM1") { Price = "$9.99" });
-            addOnService.Configure(AddOnKeys.Monthly5, new SubscriptionAddOn("9NS7RZG3GS55") { Price = "$19.99" });
+                addOnService.Configure(AddOnKeys.Monthly1, new SubscriptionAddOn("9P2WC0V906ZL") { Price = "$0.99" });
+                addOnService.Configure(AddOnKeys.Monthly2, new SubscriptionAddOn("9PLBZKNFTCL0") { Price = "$2.99" });
+                addOnService.Configure(AddOnKeys.Monthly3, new SubscriptionAddOn("9PJ1PJ9SHQ2G") { Price = "$4.99" });
+                addOnService.Configure(AddOnKeys.Monthly4, new SubscriptionAddOn("9P17MRR2QMM1") { Price = "$9.99" });
+                addOnService.Configure(AddOnKeys.Monthly5, new SubscriptionAddOn("9NS7RZG3GS55") { Price = "$19.99" });
 
-            addOnService.Configure(AddOnKeys.Durable1, new DurableAddOn("9MX7P8CB4QF0") { Price = "$0.99" });
-            addOnService.Configure(AddOnKeys.Durable2, new DurableAddOn("9N70G0L272B5") { Price = "$2.99" });
-            addOnService.Configure(AddOnKeys.Durable3, new DurableAddOn("9N0JKP3QV4GT") { Price = "$4.99" });
-            addOnService.Configure(AddOnKeys.Durable4, new DurableAddOn("9PH8KSXPB3GR") { Price = "$9.99" });
-            addOnService.Configure(AddOnKeys.Durable5, new DurableAddOn("9NXW4694NSK5") { Price = "$19.99" });
+                addOnService.Configure(AddOnKeys.Durable1, new DurableAddOn("9MX7P8CB4QF0") { Price = "$0.99" });
+                addOnService.Configure(AddOnKeys.Durable2, new DurableAddOn("9N70G0L272B5") { Price = "$2.99" });
+                addOnService.Configure(AddOnKeys.Durable3, new DurableAddOn("9N0JKP3QV4GT") { Price = "$4.99" });
+                addOnService.Configure(AddOnKeys.Durable4, new DurableAddOn("9PH8KSXPB3GR") { Price = "$9.99" });
+                addOnService.Configure(AddOnKeys.Durable5, new DurableAddOn("9NXW4694NSK5") { Price = "$19.99" });
 #else
-            addOnService.Configure(AddOnKeys.Monthly1, new SubscriptionAddOn("9P8M7BX63QVX") { Price = "$0.99" });
-            addOnService.Configure(AddOnKeys.Monthly2, new SubscriptionAddOn("9NC3R3HRVT85") { Price = "$2.99" });
-            addOnService.Configure(AddOnKeys.Monthly3, new SubscriptionAddOn("9N5C9LN7LLCF") { Price = "$4.99" });
-            addOnService.Configure(AddOnKeys.Monthly4, new SubscriptionAddOn("9PMBNNLX4P2Q") { Price = "$9.99" });
-            addOnService.Configure(AddOnKeys.Monthly5, new SubscriptionAddOn("9PDZTR16PXLQ") { Price = "$19.99" });
+                addOnService.Configure(AddOnKeys.Monthly1, new SubscriptionAddOn("9P8M7BX63QVX") { Price = "$0.99" });
+                addOnService.Configure(AddOnKeys.Monthly2, new SubscriptionAddOn("9NC3R3HRVT85") { Price = "$2.99" });
+                addOnService.Configure(AddOnKeys.Monthly3, new SubscriptionAddOn("9N5C9LN7LLCF") { Price = "$4.99" });
+                addOnService.Configure(AddOnKeys.Monthly4, new SubscriptionAddOn("9PMBNNLX4P2Q") { Price = "$9.99" });
+                addOnService.Configure(AddOnKeys.Monthly5, new SubscriptionAddOn("9PDZTR16PXLQ") { Price = "$19.99" });
 
-            addOnService.Configure(AddOnKeys.Durable1, new DurableAddOn("9N1KH0N44V71") { Price = "$0.99" });
-            addOnService.Configure(AddOnKeys.Durable2, new DurableAddOn("9PDPL8HVBRQP") { Price = "$2.99" });
-            addOnService.Configure(AddOnKeys.Durable3, new DurableAddOn("9N2Q823F2H2T") { Price = "$4.99" });
-            addOnService.Configure(AddOnKeys.Durable4, new DurableAddOn("9PPCL2KF3Z66") { Price = "$9.99" });
-            addOnService.Configure(AddOnKeys.Durable5, new DurableAddOn("9MXW1MMM4GH8") { Price = "$19.99" });
+                addOnService.Configure(AddOnKeys.Durable1, new DurableAddOn("9N1KH0N44V71") { Price = "$0.99" });
+                addOnService.Configure(AddOnKeys.Durable2, new DurableAddOn("9PDPL8HVBRQP") { Price = "$2.99" });
+                addOnService.Configure(AddOnKeys.Durable3, new DurableAddOn("9N2Q823F2H2T") { Price = "$4.99" });
+                addOnService.Configure(AddOnKeys.Durable4, new DurableAddOn("9PPCL2KF3Z66") { Price = "$9.99" });
+                addOnService.Configure(AddOnKeys.Durable5, new DurableAddOn("9MXW1MMM4GH8") { Price = "$19.99" });
 #endif
-            SimpleIoc.Default.Register<IAddOnService>(() => addOnService);
+                return addOnService;
+            });
 
             // Register Icon Resource
             var iconResources = new ResourceService<Uri>();
 
             // Register Helpers
-            SimpleIoc.Default.Register<ITileIconHelper, TileIconHelper>();
+            SimpleIoc.Default.TryRegister<ITileIconHelper, TileIconHelper>();
 
             // Register NittyGritty Services
-            SimpleIoc.Default.Register<IClipboardService, ClipboardService>();
-            SimpleIoc.Default.Register<IShareService, ShareService>();
-            SimpleIoc.Default.Register<IThemeService, ThemeService>();
-            SimpleIoc.Default.Register<IShortcutService, ShortcutService>();
-            SimpleIoc.Default.Register<IContactService, ContactService>();
+            SimpleIoc.Default.TryRegister<IClipboardService, ClipboardService>();
+            SimpleIoc.Default.TryRegister<IShareService, ShareService>();
+            SimpleIoc.Default.TryRegister<IThemeService, ThemeService>();
+            SimpleIoc.Default.TryRegister<IShortcutService, ShortcutService>();
+            SimpleIoc.Default.TryRegister<IContactService, ContactService>();
         }
 
         public override INavigationService GetNavigationService(string key)
@@ -102,6 +108,30 @@ namespace JumpPoint.Uwp.Helpers
             }
             return SimpleIoc.Default.GetInstance<INavigationService>(key);
         }
+    
+    }
+
+    public static class SimpleIocExtensions
+    {
+
+        public static void TryRegister<TService>(this SimpleIoc ioc, Func<TService> factory) where TService : class
+        {
+            if (!ioc.IsRegistered<TService>())
+            {
+                ioc.Register<TService>(factory);
+            }
+        }
+
+        public static void TryRegister<TService, TImplementation>(this SimpleIoc ioc)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            if (!ioc.IsRegistered<TService>())
+            {
+                ioc.Register<TService, TImplementation>();
+            }
+        }
+
     }
 
 }
