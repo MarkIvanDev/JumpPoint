5d07709ef628ad70b40a0a5d6777adb090835183
Mark Ivan Basto <markivan.developer@outlook.com>
2023-01-24 15:58:02 +0800

    fix drives crash

--- JumpPoint/JumpPoint.ViewModels/DrivesViewModel.cs
+++ JumpPoint/JumpPoint.ViewModels/DrivesViewModel.cs
@@ -10,6 +10,7 @@ using JumpPoint.Platform.Items.PortableStorage;
 using JumpPoint.Platform.Models;
 using JumpPoint.Platform.Models.Extensions;
 using JumpPoint.Platform.Services;
+using Nito.AsyncEx;
 using NittyGritty.Services.Core;
 using MainThread = Xamarin.Essentials.MainThread;
 
@@ -17,24 +18,29 @@ namespace JumpPoint.ViewModels
 {
     public class DrivesViewModel : ShellContextViewModelBase
     {
+        private readonly AsyncLock mutexDrives;
+
         public DrivesViewModel(IShortcutService shortcutService, AppSettings appSettings) : base(shortcutService, appSettings)
         {
-
+            mutexDrives = new AsyncLock();
         }
 
         public override bool HasCustomGrouping => true;
 
         protected override async Task Refresh(CancellationToken token)
         {
-            var drives = await StorageService.GetDrives();
-            Items.AddRange(drives);
-            token.ThrowIfCancellationRequested();
-
-            for (int i = 0; i < drives.Count; i++)
+            using (await mutexDrives.LockAsync())
             {
+                var drives = await StorageService.GetDrives();
+                Items.AddRange(drives);
                 token.ThrowIfCancellationRequested();
-                await JumpPointService.Load(drives[i]);
-                ProgressInfo.Update(Items.Count, i + 1, string.Empty);
+
+                for (int i = 0; i < drives.Count; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    await JumpPointService.Load(drives[i]);
+                    ProgressInfo.Update(Items.Count, i + 1, string.Empty);
+                }
             }
         }
 
@@ -47,41 +53,44 @@ namespace JumpPoint.ViewModels
 
         private async void PortableStorageService_PortableDriveCollectionChanged(object sender, PortableDriveCollectionChangedEventArgs e)
         {
-            switch (e.Action)
+            using (await mutexDrives.LockAsync())
             {
-                case NotifyCollectionChangedAction.Add:
-                case NotifyCollectionChangedAction.Replace:
-                    await MainThread.InvokeOnMainThreadAsync(async () =>
-                    {
-                        var item = Items.OfType<PortableDrive>().FirstOrDefault(i => i.DeviceId == e.DeviceId);
-                        await JumpPointService.Load(e.Drive);
-                        if (item != null)
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                    case NotifyCollectionChangedAction.Replace:
+                        await MainThread.InvokeOnMainThreadAsync(async () =>
                         {
-                            var index = Items.IndexOf(item);
-                            Items[index] = e.Drive;
-                        }
-                        else
-                        {
-                            Items.Add(e.Drive);
-                        }
-                    });
-                    break;
+                            var item = Items.OfType<PortableDrive>().FirstOrDefault(i => i.DeviceId == e.DeviceId);
+                            await JumpPointService.Load(e.Drive);
+                            if (item != null)
+                            {
+                                var index = Items.IndexOf(item);
+                                Items[index] = e.Drive;
+                            }
+                            else
+                            {
+                                Items.Add(e.Drive);
+                            }
+                        });
+                        break;
 
-                case NotifyCollectionChangedAction.Remove:
-                    await MainThread.InvokeOnMainThreadAsync(() =>
-                    {
-                        var item = Items.OfType<PortableDrive>().FirstOrDefault(i => i.DeviceId == e.DeviceId);
-                        if (item != null)
+                    case NotifyCollectionChangedAction.Remove:
+                        await MainThread.InvokeOnMainThreadAsync(() =>
                         {
-                            Items.Remove(item);
-                        }
-                    });
-                    break;
+                            var item = Items.OfType<PortableDrive>().FirstOrDefault(i => i.DeviceId == e.DeviceId);
+                            if (item != null)
+                            {
+                                Items.Remove(item);
+                            }
+                        });
+                        break;
 
-                case NotifyCollectionChangedAction.Move:
-                case NotifyCollectionChangedAction.Reset:
-                default:
-                    break;
+                    case NotifyCollectionChangedAction.Move:
+                    case NotifyCollectionChangedAction.Reset:
+                    default:
+                        break;
+                }
             }
         }
 
