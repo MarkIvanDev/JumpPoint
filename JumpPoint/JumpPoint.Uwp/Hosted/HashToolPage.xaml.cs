4cee98571fbec58eb1e249181e24ea825cf0ec08
Mark Ivan Basto <markivan.developer@outlook.com>
2023-03-01 16:53:13 +0800

    add copy button to hash tools

--- JumpPoint/JumpPoint.Uwp/Hosted/HashToolPage.xaml.cs
+++ JumpPoint/JumpPoint.Uwp/Hosted/HashToolPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using JumpPoint.Uwp.Controls;
 using JumpPoint.ViewModels;
 using JumpPoint.ViewModels.Hosted;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Essentials;
+
 namespace JumpPoint.Uwp.Hosted
 {
     public sealed partial class HashToolPage : PageBase
@@ -12,5 +15,13 @@ namespace JumpPoint.Uwp.Hosted
         }
 
         public HashToolViewModel ViewModel => DataContext as HashToolViewModel;
+
+        private async void OnCopy(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (sender is Button button && button.DataContext != null)
+            {
+                await Clipboard.SetTextAsync(button.DataContext.ToString());
+            }
+        }
     }
 }
